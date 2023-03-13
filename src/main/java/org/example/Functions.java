8c4a3bfbe41a3747fe285be2cb6dafa9b0e00831
222150 Witczak Kamil <222150@edu.p.lodz.pl>
2023-03-13 13:36:57 +0100

    Interface

---
+++ src/main/java/org/example/Functions.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import static java.lang.Math.pow;
+
+public class Functions {
+    private double a;
+    private double b;
+    double epsilon;
+
+    int funkcja;
+
+    int stop;
+
+    int ite;
+
+
+    public Functions(double a, double b, int funkcja, int stop, double epsilon) {
+        this.a = a;
+        this.b = b;
+        this.epsilon = epsilon;
+        this.funkcja = funkcja;
+        this.stop = stop;
+    }
+
+    public Functions(double a, double b, int funkcja, int stop, int ite) {
+        this.a = a;
+        this.b = b;
+        this.funkcja = funkcja;
+        this.stop = stop;
+        this.ite = ite;
+    }
+
+    public double f(double x) {
+        return pow(x, 3) + pow(x, 2) - 3;
+    }
+
+    public double bis() {
+        double sr;
+        int kroki = 0;
+        if (f(a) * f(b) < 0) {
+            sr = a;
+            if (stop == 1) {
+                while ((b - a) > epsilon) {
+                    sr = (a + b) / 2;
+                    if (f(a) * f(sr) < 0) {
+                        b = sr;
+                    } else if (f(b) * f(sr) < 0) {
+                        a = sr;
+                    } else break;
+                }
+            } else if (stop == 2) {
+                while (kroki == ite) {
+                    sr = (a + b) / 2;
+                    if (f(a) * f(sr) < 0) {
+                        b = sr;
+                    } else if (f(b) * f(sr) < 0) {
+                        a = sr;
+                    } else break;
+                    kroki++;
+                }
+            }
+        } else {
+            sr = 0;
+        }
+        return sr;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,38 @@ import org.jfree.data.statistics.HistogramDataset;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Bisekcji bisekcji = new Bisekcji(-6,6,0.00000000001, 200);
-        double res = bisekcji.bis();
+        Scanner scan = new Scanner(System.in);
+        Functions functions = null;
+        System.out.println("Proszę wybrać funkcję:");
+        int chooseFunction = scan.nextInt();
+        System.out.println("Proszę podać dolny przedział:");
+        double chooseDoubleLow = scan.nextDouble();
+        System.out.println("Proszę podać górny przedział:");
+        double chooseDoubleHigh = scan.nextDouble();
+        System.out.println("Proszę wybrać kryterium stopu:");
+        System.out.println("1. Dokładność");
+        System.out.println("2. Liczba iteracji");
+        int chooseStop = scan.nextInt();
+        double chooseEps;
+        int chooseIte;
+        if (chooseStop == 1) {
+            System.out.println("Proszę podać epsilon:");
+            chooseEps = scan.nextDouble();
+            functions = new Functions(chooseDoubleLow, chooseDoubleHigh, chooseFunction, chooseStop, chooseEps);
+        } else if (chooseStop == 2) {
+            System.out.println("Proszę podać ilość iteracji:");
+            chooseIte = scan.nextInt();
+            functions = new Functions(chooseDoubleLow, chooseDoubleHigh, chooseFunction, chooseStop, chooseIte);
+        }
+        double res = functions.bis();
         System.out.println(res);
+        Functions f = new Functions(-6,6,1,2,200);
+        System.out.println(f.bis());
+        Functions f2 = new Functions(-6,6,1,1,0.000000000001);
+        System.out.println(f2.bis());
     }
 }
