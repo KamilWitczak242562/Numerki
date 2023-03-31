7ee914773a1a6550ffeb24aad7b02fbea7468c91
Kamil Witczak <222150@edu.p.lodz.pl>
2023-03-31 14:52:36 +0200

    Zad 2 start

--- src/main/java/org/example/Func.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface Func {
-    double operation(double x);
-    double derivative(double x);
-    String funcToString();
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,57 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        Func func = null;
-        Functions functions = null;
-        System.out.println("Proszę wybrać funkcję:");
-        System.out.println("1. Wielomian");
-        System.out.println("2. Trygonometryczna");
-        System.out.println("3. Wykładnicza");
-        System.out.println("4. Złożona");
-        int chooseFunction = scan.nextInt();
-        switch (chooseFunction) {
-            case 1:
-                func = new Polynomial();
-                break;
-            case 2:
-                func = new Trigonometric();
-                break;
-            case 3:
-                func = new Exponent();
-                break;
-            case 4:
-                func = new Complex();
-                break;
-        }
-        System.out.println("Proszę podać dolny przedział:");
-        double chooseDoubleLow = scan.nextDouble();
-        System.out.println("Proszę podać górny przedział:");
-        double chooseDoubleHigh = scan.nextDouble();
-        System.out.println("Proszę wybrać kryterium stopu:");
-        System.out.println("1. Dokładność");
-        System.out.println("2. Liczba iteracji");
-        int chooseStop = scan.nextInt();
-        double chooseEps;
-        int chooseIte;
-        if (chooseStop == 1) {
-            System.out.println("Proszę podać epsilon:");
-            chooseEps = scan.nextDouble();
-            functions = new Functions(chooseDoubleLow, chooseDoubleHigh, func, chooseStop, chooseEps);
-        } else if (chooseStop == 2) {
-            System.out.println("Proszę podać ilość iteracji:");
-            chooseIte = scan.nextInt();
-            functions = new Functions(chooseDoubleLow, chooseDoubleHigh, func, chooseStop, chooseIte);
-        }
-        double resBis = functions.bis();
-        System.out.println("Wyniki metodą bisekcji: " + resBis);
-        double resTan = functions.tan();
-        System.out.println("Wyniki metodą stycznych: " + resTan);
 
-        Func finalFunc = func;
-        EventQueue.invokeLater(() -> {
-            var fun = new DrawFunction(chooseDoubleLow, chooseDoubleHigh, finalFunc, resBis, resTan);
-            fun.setVisible(true);
-        });
     }
 }
